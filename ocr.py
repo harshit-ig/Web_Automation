1e5f313f41c200d862d3e83bd85aa96f4b1d2630
Elite <sharshit0369@gmail.com>
2022-03-09 14:42:38 +0530

    improved ocr to remove spaces from the string to increase praformance in captcha solving

--- ocr.py
+++ ocr.py
@@ -4,5 +4,6 @@ def decode(url):
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     img = cv2.imread(url)
     text = pytesseract.image_to_string(img)
+    text = text.replace(" ", "")
     return text
 print(decode("screenshot.jpg"))
